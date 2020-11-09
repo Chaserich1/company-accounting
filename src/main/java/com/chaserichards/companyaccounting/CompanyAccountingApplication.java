2f11ac1d6da9d2c331e93a548e69e33f51c6860d
Richards <CNR1103@daugherty.com>
2020-11-09 16:21:05 -0600

    Basic setup using spring initializer

---
+++ src/main/java/com/chaserichards/companyaccounting/CompanyAccountingApplication.java
@@ -0,0 +1,13 @@
+package com.chaserichards.companyaccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyAccountingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chaserichards/companyaccounting/CompanyAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaserichards.companyaccounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyAccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
