afca13e36d1513c919cf948cfe9b2ccd0a1efac2
Richards <CNR1103@daugherty.com>
2020-11-10 08:52:55 -0600

    Updates

--- src/main/java/com/chaserichards/api/CompanyAccountingApplication.java
+++ src/main/java/com/chaserichards/api/CompanyAccountingApplication.java
@@ -1,4 +1,4 @@
-package com.chaserichards.companyaccounting;
+package com.chaserichards.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/chaserichards/api/CompanyAccountingApplicationTests.java
+++ src/test/java/com/chaserichards/api/CompanyAccountingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.chaserichards.companyaccounting;
+package com.chaserichards.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
